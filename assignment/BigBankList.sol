9b8958e8788e04013da47344e5c9468e24ff14e6
tuwenchu <167046068+tuwenchu@users.noreply.github.com>
2024-04-22 14:24:11 +0800

    Update BigBankList.sol

--- assignment/BigBankList.sol
+++ assignment/BigBankList.sol
@@ -14,7 +14,7 @@ pragma solidity ^0.8.0;
     // / 修饰符，用于限制最小存款金额
     modifier minimumDeposit(uint256 minAmount) {
         require(msg.value >= minAmount, "Deposit amount must be greater than or equal to 0.001 ether.");
-        _; // Continue with the function if the condition is met
+        _;
     }
     // 接收函数 internal
     function deposit() public payable minimumDeposit(0.001 ether)  {
@@ -37,11 +37,6 @@ pragma solidity ^0.8.0;
         // 转账给合约所有者
         payable(msg.sender).transfer(amount);
         emit Withdrawal (msg.sender ,amount );
-    }
-    // 转移管理员
-    function transferOwner( address newOwner) public {
-        require(msg.sender == owner, 'You are not an administrator and cannot transfer your identity');
-        owner = newOwner;
     }
          // 前三位用户地址和存款金额
     address[3] public topThreeUsers;  //地址
@@ -74,12 +69,8 @@ contract BigBank is Bank {
         // 调用父合约的存款函数
         super.deposit();
     }
-    // BigBank 合约支持转移管理员
-    function transferBankOwner(address _newOwner) public {
-        super.transferOwner(_newOwner);
-    }
     // 把 BigBank 的管理员转移给 Ownable 合约
-    function transferOwnableOwner(address _newOwner) public {
+    function transferOwner(address _newOwner) public {
          require(msg.sender == owner, 'Not owner');
          require(_newOwner != address(0), "0 address");
         owner = _newOwner;
@@ -92,21 +83,16 @@ contract Ownable  {
       constructor(address _bigBank){
         bigBank = BigBank(payable(_bigBank));
       }
+      modifier onlyOwner (){
+        require(msg.sealed == owner, 'notowner '); _;
+      }
        // 接收 ETH 的默认回调函数
     receive() external payable {}
-   function withdrawal(uint256 amount) public {
+   function withdrawal(uint256 amount) public onlyOwner{
     bigBank.withdrawal(amount);
    }
    //身份转移
    function transferBigBankOwner(address _newOwner) public{
-    bigBank.transferOwnableOwner(_newOwner);
+    bigBank.transferOwner(_newOwner);
    }
 }
-// 测试
-// 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4. 输入1
-// 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2  输入2 第二次输入4
-// 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db  输入3。 
-
-// 第一 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
-// 第二 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
-// 第三 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2

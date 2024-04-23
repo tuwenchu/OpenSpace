e9b2cd226fcf31e344a6c527159a1e18e36b454e
tuwenchu <167046068+tuwenchu@users.noreply.github.com>
2024-04-23 14:03:50 +0800

    Update BaseERC20.sol

--- assignment/Token/BaseERC20.sol
+++ assignment/Token/BaseERC20.sol
@@ -74,3 +74,30 @@ contract BaseERC20 {
         return allowances[_owner][_spender];
     }
 }
+
+设置代币名称（name）："BaseERC20"
+设置代币符号（symbol）："BERC20"
+设置Token小位数：18
+设置Token总体（totalSupply）:100,000,000
+允许任何人查看任何地址的代币余额（balanceOf）
+允许Token的所有者将他们的Token发送给任何人（转账）；转帐超出余额时发送异常(require)，并显示错误消息“ERC20：转账金额超出余额”。
+允许 Token 的所有者批准某个地址消费他们的一部分 Token（批准）
+允许任何人查看一个地址可以从其他账户中转账的代币数量（允许）
+允许被授权的地址消费他们被授权的Token数量（transferFrom）；
+转帐超出授权数量时发送异常(require)，异常信息：“ERC20: 转账金额超出余额”
+转帐超出授权数量时发送异常(require)，异常消息：“ERC20：转账金额超出限额”。
+注意：
+在编写合约时，需要遵循 ERC20 标准，此外还需要考虑安全性，确保转账和授权功能在任何时候都能正常运行无误。
+代码模板中已包含基础框架，只需要在标记为“Write your code here”的位置编写了您的代码。不要去修改现有内容！
+
+
+
+
+
+
+
+
+
+
+
+
